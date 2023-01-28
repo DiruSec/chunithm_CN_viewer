4551906c4832d517fc5755f9017b8d0caef0045c
DiruSec <gripeais@gmail.com>
2023-01-28 16:59:50 +0800

    fix recursion issue

--- common.js
+++ common.js
@@ -1,4 +1,14 @@
-document.write('<script src="https://chunithm.wahlap.com/mobile/js/common.js?ver=1.00"></script>')
+var chunicommon = document.createElement('script')
+chunicommon.src="https://chunithm.wahlap.com/mobile/js/common.js?ver=1.00&_t="+Date.now()
+document.body.append(chunicommon)
 if (location.href.match(/playlog/) || (location.href.match(/musicGenre/) && window.$(".box01_title span").length)) {
-    document.write('<script src="https://cdn.jsdelivr.net/gh/DiruSec/chunithm_CN_viewer@master/chunithm.js"></script>')
+  var button = document.createElement('div')
+  button.innerHTML = '<div id="btn-activate" style="box-sizing: content-box; position: fixed; left: .4rem; bottom: .4rem; width: 7.2rem; height: 1.6rem; border-radius: .4rem; background: linear-gradient(#4b5e71, #000000); line-height: 1.6rem; padding: .4rem .8rem; border: .2rem solid #fefeee; color: #fff; font-weight: 500;">打开查分器</div>'
+  button.addEventListener("click", function() {
+    var chuni = document.createElement('script')
+    chuni.src = "https://cdn.jsdelivr.net/gh/DiruSec/chunithm_CN_viewer@master/chunithm.js"
+    document.body.append(chuni)
+    button.innerText = "请等待…"
+  }, { once: true })
+  document.body.append(button)
 }
