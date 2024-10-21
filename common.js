1cb08fce44da21d0d39ea0839e2bf5c190a3c093
DiruSec <gripeais@gmail.com>
2024-10-21 13:57:57 +0800

    updatelink

--- common.js
+++ common.js
@@ -6,7 +6,7 @@ if (location.href.match(/playlog/) || (location.href.match(/musicGenre/) && wind
   button.innerHTML = '<div id="btn-activate" style="box-sizing: content-box; position: fixed; left: .4rem; bottom: .4rem; width: 7.2rem; height: 1.6rem; border-radius: .4rem; background: linear-gradient(#4b5e71, #000000); line-height: 1.6rem; padding: .4rem .8rem; border: .2rem solid #fefeee; color: #fff; font-weight: 500;">打开查分器</div>'
   button.addEventListener("click", function() {
     var chuni = document.createElement('script')
-    chuni.src = "https://cdn.jsdelivr.net/gh/DiruSec/chunithm_CN_viewer@master/chunithm.js"
+    chuni.src = "https://raw.githubusercontent.com/DiruSec/chunithm_CN_viewer/refs/heads/main/chunithm.js"
     document.body.append(chuni)
     button.innerText = "请等待…"
   }, { once: true })
